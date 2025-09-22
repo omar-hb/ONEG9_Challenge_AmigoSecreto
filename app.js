d9d236a264460abe8591e4426bc2a7782537ddf5
omar-hb <omar.hb.edu@gmail.com>
2025-09-21 22:41:26 -0600

    Array amigos Y Función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* 
+ONE G9 - Alura Latam
+Elaborado por: Omar Hernández Bravo - omar.hb.edu(AT)gmail.com
+*/
+
+// Se declara el arreglo para alojar los nombres de los amigos que participan del sorteo
+let amigos = [];
+
+//Se declara función agregarAmigo para agregar nombres al array
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let listaAmigos = document.getElementById('listaAmigos');
+    let nombre = input.value.trim();
+}    
