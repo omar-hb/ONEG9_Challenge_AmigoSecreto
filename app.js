38488b10d7a01880994c2f154c1523ad261803ae
omar-hb <omar.hb.edu@gmail.com>
2025-09-21 23:05:02 -0600

    Actualización lista de nombres

--- app.js
+++ app.js
@@ -12,4 +12,27 @@ function agregarAmigo() {
     let input = document.getElementById('amigo');
     let listaAmigos = document.getElementById('listaAmigos');
     let nombre = input.value.trim();
-}    
+
+// Se verifica que no se ingrese un nombre vacío
+    if (!nombre) {
+        alert("Nombre vacío. Ingresa un nombre para agregarlo a la lista");
+        input.focus();
+        return;
+    }
+
+// Se valida número máximo de nombres
+    if (amigos.length >= 5) {
+        alert(`No se pueden agregar más amigos. Máximo ${amigos.length}.`);
+        input.value = '';
+        input.focus();
+        return;
+    }
+
+    amigos.push(nombre);
+    const lista = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    lista.appendChild(li);
+    input.value = '';
+    input.focus();
+}
